1b5fa6ab784215f731ca3dc0370a9589123214b6
Sucharita993 <84003518+Sucharita993@users.noreply.github.com>
2021-05-13 14:11:41 +0530

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,19 @@
+class Pattern
+{
+	public static void main(String[] args)
+	{
+		int n=5,i,j,kl=5;
+		for(i=1;i<=n;i++)
+		{
+		for(j=1;j<=l;j++)
+		{
+		System.out.print(" ");
+		}
+		for(k=1;k<=2*i-1;k++)
+		{
+		System.out.print("*");
+		}
+		l--;
+		}
+	}
+}
